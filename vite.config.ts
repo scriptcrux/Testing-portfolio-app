0750446fc517ff8c32db886ad33d2565c5d2cddd
Scriptcrux <scriptcrux.tech@gmail.com>
2025-02-15 21:31:49 +0530

    adding cname

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-test',
-  // base: './',
+  // base: '/Testing-portfolio-app',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
